af7d1d2345ed35f4d11bdf742984186b57102635
bodeng <drawing@users.noreply.github.com>
2015-08-13 20:31:17 +0800

    add README

---
+++ encoder/README.md
@@ -0,0 +1,47 @@
+
+# DataStream
+
+simple binary encode and decode function
+
+# Usage
+
+## OutDataStream
+
+OutDataStream out;
+
+uint8_t i8 = 10;
+uint16_t i16 = 0x1234;
+std::string s = "abc";
+
+out << i8 << s << i16;
+
+std::string res = out.data();
+
+## InDataStream
+
+InDataStream in(res);
+
+uint8_t oi8;
+uint16_t oi16;
+uint8_t c;
+
+std::string ostr;
+in >> oi8 >> manip::in_string(ostr, 3) >> oi16;
+
+## ProtoBuf
+
+test_node::ProtoNode n1;
+n1.set_id(1);
+n1.set_value(11);
+
+OutDataStream out;
+out << (uint16_t)n1.ByteSize() << manip::protobuf(n1);
+
+std::string res = out.data();
+
+test_node::ProtoNode n2;
+
+InDataStream in(res);
+uint16_t sz = 0;
+in >> sz;
+in >> manip::protobuf(n2, sz);
--- tests/encoder/data_stream_pb_test.cpp
+++ tests/encoder/data_stream_pb_test.cpp
@@ -29,3 +29,35 @@ TEST(DataStream, pb)
 	ASSERT_TRUE(n1.value() == n2.value());
 }
 
+TEST(DataStream, pb2)
+{
+	test_node::ProtoNode n1;
+	test_node::ProtoNode n2;
+	n1.set_id(1);
+	n1.set_value(11);
+	n2.set_id(2);
+	n2.set_value(12);
+
+	OutDataStream out;
+	out << (uint16_t)n1.ByteSize() << (uint16_t)n2.ByteSiez()
+		<< manip::protobuf(n1) << manip::protobuf(n2);
+
+	std::string res = out.data();
+
+	test_node::ProtoNode r1;
+	test_node::ProtoNode r2;
+
+	InDataStream in(res);
+	uint16_t sz1 = 0;
+	uint16_t sz2 = 0;
+	in >> sz1 >> sz2;
+	in >> manip::protobuf(r1, sz1) >> manip::protobuf(r2, sz2);
+
+	ASSERT_TRUE(sz1 == n1.ByteSize());
+	ASSERT_TRUE(sz2 == n2.ByteSize());
+	ASSERT_TRUE(n1.id() == r1.id());
+	ASSERT_TRUE(n2.id() == r2.id());
+	ASSERT_TRUE(n1.value() == r1.value());
+	ASSERT_TRUE(n2.value() == r2.value());
+}
+
